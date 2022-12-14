3d36a4873eb7ee7fdac5ad30ad965ba1d7f6e780
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2022-12-14 12:35:53 +0200

    Fix Image

--- src/VladislavAntonyuk/Models/Article.cs
+++ src/VladislavAntonyuk/Models/Article.cs
@@ -6,7 +6,7 @@ public class Article : BaseEntity
 	public string? Content { get; set; }
 	public string? Description { get; set; }
 	public string? Keywords { get; set; }
-	public string? ImageUrl { get; set; }
+	public string? Image { get; set; }
     public DateOnly Created { get; set; }
     public string? CategoryName { get; set; }
 }
--- src/VladislavAntonyuk/Services/ArticlesService.cs
+++ src/VladislavAntonyuk/Services/ArticlesService.cs
@@ -20,36 +20,45 @@ class ArticlesService : IArticlesService
     }
     public async Task<List<Article>> GetArticles(string? categoryName = null, string? searchParameter = null)
     {
-        var categories = await _httpClient.GetFromJsonAsync<Category[]>("data/categories.json");
+        var categories = await _httpClient.GetFromJsonAsync<IEnumerable<Category>>("data/categories.json");
         if (categories is null)
         {
             return new List<Article>();
         }
 
-        var articles = string.IsNullOrEmpty(categoryName) ?
-            categories.SelectMany(x => x.Articles) :
-            categories.Where(x => x.Name.Equals(categoryName, StringComparison.OrdinalIgnoreCase)).SelectMany(x => x.Articles);
+        if (!string.IsNullOrEmpty(categoryName))
+        {
+            categories = categories.Where(x => x.Name.Equals(categoryName, StringComparison.OrdinalIgnoreCase));
+        }
 
+        var articles = categories.SelectMany(x => x.Articles, (category, article) =>
+        {
+            article.CategoryName = category.Name;
+            return article;
+        });
+        
         if (!string.IsNullOrEmpty(searchParameter))
         {
             articles = articles.Where(x => x.Name.Contains(searchParameter, StringComparison.OrdinalIgnoreCase));
         }
 
-        return articles.ToList();
+        return articles.OrderByDescending(x=> x.Created).ToList();
     }
 
     public async Task<Article?> GetArticle(string articleName)
     {
         var categories = await _httpClient.GetFromJsonAsync<Category[]>("data/categories.json");
-
-        var category = categories?.FirstOrDefault(x => x.Articles.FirstOrDefault(y => y.Name.Equals(articleName, StringComparison.OrdinalIgnoreCase)) != null);
-
-        if (category is null)
+        if (categories is null)
         {
             return null;
         }
-
-        var article = category.Articles.SingleOrDefault(x => x.Name.Equals(articleName, StringComparison.OrdinalIgnoreCase));
+        
+        var article = categories.SelectMany(x => x.Articles, (category, article) =>
+        {
+            article.CategoryName = category.Name;
+            return article;
+        }).SingleOrDefault(x => x.Name.Equals(articleName, StringComparison.OrdinalIgnoreCase));
+        
         if (article is null)
         {
             return null;
@@ -62,7 +71,6 @@ class ArticlesService : IArticlesService
         }
 
         article.Content = await contentResponseMessage.Content.ReadAsStringAsync();
-        article.CategoryName = category.Name;
         return article;
     }
 
@@ -73,11 +81,18 @@ class ArticlesService : IArticlesService
         {
             return new List<Article>();
         }
+        
+        var articles = categories.SelectMany(x => x.Articles, (category, article) =>
+        {
+            article.CategoryName = category.Name;
+            return article;
+        }).ToList();
 
-        var category = categories.FirstOrDefault(x => x.Articles.FirstOrDefault(y => y.Name.Equals(articleName, StringComparison.OrdinalIgnoreCase)) != null);
-        var articles = string.IsNullOrEmpty(category?.Name) ?
-            categories.SelectMany(x => x.Articles) :
-            category.Articles;
+        if (!string.IsNullOrEmpty(articleName))
+        {
+            var categoryName = articles.FirstOrDefault(x => x.Name == articleName)?.CategoryName;
+            articles = articles.Where(x => x.CategoryName == categoryName).ToList();
+        }
 
         var allSuggestions = articles.Where(x => !x.Name.EndsWith(articleName, StringComparison.OrdinalIgnoreCase)).OrderBy(q => q.Id).ToList();
 
