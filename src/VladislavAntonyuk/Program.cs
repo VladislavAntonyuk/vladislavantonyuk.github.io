7338312aa7ed1e6c421062ddd7a1ae55fc25b26f
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2023-01-05 20:28:09 +0200

    remove update service

--- src/VladislavAntonyuk/Program.cs
+++ src/VladislavAntonyuk/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using Shared;
 using StardustDL.RazorComponents.Markdown;
-using Toolbelt.Blazor.Extensions.DependencyInjection;
 using VladislavAntonyuk;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -20,7 +19,6 @@ static void ConfigureServices(IServiceCollection services, string baseAddress)
 	services.AddScoped<IProjectsService, ProjectsService>();
 	services.AddMudServices();
 	services.AddMarkdownComponent();
-	services.AddPWAUpdater();
 
 	services.AddScoped(sp => new HttpClient
 	{
