d53154944d70bcb6f20c8f99cb3a3ce891338048
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2023-01-05 19:57:40 +0200

    add wasm prerender

--- src/VladislavAntonyuk/Program.cs
+++ src/VladislavAntonyuk/Program.cs
@@ -7,18 +7,23 @@ using Toolbelt.Blazor.Extensions.DependencyInjection;
 using VladislavAntonyuk;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.Services.AddShared();
-builder.Services.AddScoped<IArticlesService, ArticlesService>();
-builder.Services.AddScoped<IProjectsService, ProjectsService>();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddMudServices();
-builder.Services.AddMarkdownComponent();
-builder.Services.AddPWAUpdater();
-
-builder.Services.AddScoped(sp => new HttpClient
-{
-	BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-});
+ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
 
 await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection services, string baseAddress)
+{
+	services.AddShared();
+	services.AddScoped<IArticlesService, ArticlesService>();
+	services.AddScoped<IProjectsService, ProjectsService>();
+	services.AddMudServices();
+	services.AddMarkdownComponent();
+	services.AddPWAUpdater();
+
+	services.AddScoped(sp => new HttpClient
+	{
+		BaseAddress = new Uri(baseAddress)
+	});
+}
