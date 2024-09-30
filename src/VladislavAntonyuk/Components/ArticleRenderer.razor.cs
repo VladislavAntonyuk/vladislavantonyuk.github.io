e3b54e048ac05d9f43f4a93c05b31a10a792b60b
Vladislav Antonyuk <vladislav.antonyuk@dataart.com>
2024-09-30 17:04:53 +0300

    Clean up css and js

--- src/VladislavAntonyuk/Components/ArticleRenderer.razor.cs
+++ src/VladislavAntonyuk/Components/ArticleRenderer.razor.cs
@@ -1,26 +1,10 @@
 ﻿namespace VladislavAntonyuk.Components;
 
 using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
 
-public partial class ArticleRenderer(IJSRuntime jsRuntime) : VladislavAntonyukBaseComponent
+public partial class ArticleRenderer : VladislavAntonyukBaseComponent
 {
 	[Parameter]
 	[EditorRequired]
 	public required string Content { get; set; }
-
-	protected override async Task OnAfterRenderAsync(bool firstRender)
-	{
-		await base.OnAfterRenderAsync(firstRender);
-		if (firstRender)
-		{
-			await Task.Delay(1000);
-			await EmbedCopyToClipboardCode();
-		}
-	}
-
-	private ValueTask EmbedCopyToClipboardCode()
-	{
-		return jsRuntime.InvokeVoidAsync("JsFunctions.embedCopyToClipboardCode");
-	}
 }
