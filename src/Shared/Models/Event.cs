90261f8ae458cea4e77aec81442a5ca59f3ee9fa
Vladislav Antonyuk <vladislav.antonyuk@dataart.com>
2024-10-13 18:51:42 +0300

    Add event image

--- src/Shared/Models/Event.cs
+++ src/Shared/Models/Event.cs
@@ -5,4 +5,5 @@ public class Event : BaseEntity
 	public required string Name { get; set; }
 	public string? Description { get; set; }
 	public string? RedirectUrl { get; set; }
+	public string? Image { get; set; }
 }
