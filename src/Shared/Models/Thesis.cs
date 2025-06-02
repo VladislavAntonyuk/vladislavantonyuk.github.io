8beeade444102d895c571551ddbdd2e9d8f6dca2
Vladislav Antonyuk <vladislav.antonyuk@dataart.com>
2025-06-02 11:07:08 +0300

    Add theses

--- src/Shared/Models/Thesis.cs
+++ src/Shared/Models/Thesis.cs
@@ -1,21 +1,11 @@
 ﻿namespace Shared.Models;
 
-public enum JournalCategory
-{
-	A,
-	B
-}
-
-public class Publication : BaseEntity
+public class Thesis : BaseEntity
 {
 	public List<string> Authors { get; set; } = [];
 	public required string Name { get; set; }
 	public string? Journal { get; set; }
-	public JournalCategory JournalCategory { get; set; }
-	public string? Volume { get; set; }
 	public string? Pages { get; set; }
 	public required string Date { get; set; }
-	public string? Doi { get; set; }
-	public List<string> Keywords { get; set; } = [];
 	public string? Link { get; set; }
 }
--- src/VladislavAntonyuk/Services/PublicationsService.cs
+++ src/VladislavAntonyuk/Services/PublicationsService.cs
@@ -1,14 +1,21 @@
 ﻿namespace VladislavAntonyuk.Services;
 
 using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Shared;
 using Shared.Models;
 
 internal class PublicationsService(HttpClient httpClient) : IPublicationsService
 {
+	private static readonly JsonSerializerOptions Options = new(JsonSerializerDefaults.Web)
+	{
+		Converters = { new JsonStringEnumConverter<JournalCategory>() }
+	};
+
 	public async Task<List<Publication>> GetArticles(string? searchParameter = null, CancellationToken cancellationToken = default)
 	{
-		var publications = await httpClient.GetFromJsonAsync<IEnumerable<Publication>>("data/publications.json", cancellationToken);
+		var publications = await httpClient.GetFromJsonAsync<IEnumerable<Publication>>("data/publications.json", Options, cancellationToken);
 		if (publications is null)
 		{
 			return [];
